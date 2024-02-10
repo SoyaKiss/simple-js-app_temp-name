6dd64eed0287f9c61ab9e4cb60034bfd89ba603b
SoyaKiss <orsolya.kiss.design@gmail.com>
2024-02-09 23:38:26 -0500

    pokeman variable defined;  3 objects per array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello world');
-
-let favoriteFood = 'cheeseburger';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Horsea', height: 4, type: ['damp','swim']},
+    {name: 'Porygon2', height: 6, type: ['trace','analytic']},
+    {name: 'Uxie', height: 3, type: ['levitate','psychic']},
+];
